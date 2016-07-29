67f83a440101bb4745e846905b6b24b688ee7a4d
Stanislav Valasek <valasek@gmail.com>
2016-07-29 23:52:04 +0200

    Create docker-clean.sh

---
+++ docker-clean.sh
@@ -0,0 +1,6 @@
+# Delete every Docker containers
+# Must be run first because images are attached to containers
+docker rm $(docker ps -a -q)
+
+# Delete every Docker image
+docker rmi $(docker images -q)
